11a11b61bd52d2a269d0c7de8f523aff42cfbac1
ADFGE\jperezr <jperezr@fiscaliaveracruz.gob.mx>
2018-09-11 22:58:33 -0500

    commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+      <div class="card-body">
+        <div id="app">
+          <menuc></menuc>
+        </div>
+        <script src="{{ asset('js/app.js')}}"></script>
+      </div>
+    </body>
+</html>
